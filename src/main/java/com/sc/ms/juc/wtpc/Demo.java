27c8a219a29bd79794f43fc2f71cdca15d9af661
shichao <shichao@souche.com>
2019-05-06 14:35:22 +0800

    添加问题排查

---
+++ src/main/java/com/sc/ms/juc/wtpc/Demo.java
@@ -0,0 +1,15 @@
+package com.sc.ms.juc.wtpc;
+
+/**
+ * @author: shichao
+ * @date: 2019/5/5
+ * @description:
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        while (true){
+            System.out.println("12345670");
+        }
+    }
+}
